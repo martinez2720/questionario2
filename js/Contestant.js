c390249ff3ab71117debec63b6c00da530b304b9
martinez2720 <105889165+martinez2720@users.noreply.github.com>
2023-01-31 12:03:09 -0300

    Add files via upload

---
+++ js/Contestant.js
@@ -0,0 +1,38 @@
+class Contestant {
+  constructor(){
+    this.index = null;
+    this.answer = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var contestantCountRef = database.ref('contestantCount');
+
+
+    contestantCountRef.on("value",(data)=>{
+      contestantCount = data.val();
+    })
+
+  
+
+    //contestantCountRef.on("value",(data)=>{
+      //contestantCount = data();
+    //})
+  }
+
+  updateCount(count){
+    database.ref("/").update({
+      contestantCount: count
+    });
+
+  
+
+    //database("/").update({
+      //contestantCount = count
+    //});
+  }
+
+ 
+
+ 
+}
